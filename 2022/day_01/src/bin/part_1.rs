0d717e7bc6d804b9b13d58232ffe463558eec97c
dan.reed <dan.reed@vistair.com>
2022-12-01 16:45:07 +0000

    Re-organise project structure

--- 2022/day_01/src/bin/part_1.rs
+++
@@ -1,9 +0,0 @@
-use day_01::get_parsed_input;
-
-fn main() -> Result<(), std::io::Error> {
-    let elves = get_parsed_input()?;
-    let sums: Vec<u32> = elves.iter().map(|elf| elf.iter().sum::<u32>()).collect();
-    println!("Max = {}", sums.iter().max().unwrap_or(&0));
-
-    Ok(())
-}
--- 2022/day_01/src/lib.rs
+++ 2022/day_01/src/lib.rs
@@ -1,7 +1,7 @@
 use std::fs::read_to_string;
 
 pub fn get_parsed_input() -> Result<Vec<Vec<u32>>, std::io::Error> {
-    let file = read_to_string("./day_01/input.txt")?;
+    let file = read_to_string("./input.txt")?;
     let lines = file.lines();
 
     let mut elves: Vec<_> = Vec::new();
@@ -23,4 +23,40 @@ pub fn get_parsed_input() -> Result<Vec<Vec<u32>>, std::io::Error> {
     }
 
     Ok(elves)
+}
+
+pub fn part_1() -> u32 {
+    let elves = get_parsed_input().expect("Failed to get parsed input");
+
+    let sums: Vec<u32> = elves.iter().map(|elf| elf.iter().sum::<u32>()).collect();
+
+    *sums.iter().max().unwrap_or(&0)
+}
+
+pub fn part_2() -> u32 {
+    let elves = get_parsed_input().expect("Failed to get parsed input");
+
+    let mut sums: Vec<u32> = elves.iter().map(|elf| elf.iter().sum::<u32>()).collect();
+    sums.sort();
+
+    let mut top_three_sum = sums.pop().unwrap_or(0);
+    top_three_sum += sums.pop().unwrap_or(0);
+    top_three_sum += sums.pop().unwrap_or(0);
+
+    top_three_sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_works() {
+        assert_eq!(part_1(), 69501)
+    }
+
+    #[test]
+    fn part_2_works() {
+        assert_eq!(part_2(), 202346)
+    }
 }
