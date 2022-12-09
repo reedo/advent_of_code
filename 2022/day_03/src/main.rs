c4dd10be28388a4a489ff4059ecf6c9c03e64148
reedo <info@reederror.com>
2022-12-09 23:41:03 +0000

    tests pass for part 1

--- 2022/day_03/src/main.rs
+++ 2022/day_03/src/main.rs
@@ -7,11 +7,39 @@ fn main() {
 }
 
 fn part_1(input: &str) -> u32 {
-    0
+    parse_input(input)
+        .iter()
+        .map(|pair| priority_of(&get_shared_char(*pair)))
+        .sum()
 }
 
 fn part_2(input: &str) -> u32 {
     0
+}
+
+fn parse_input(input: &str) -> Vec<(&str, &str)> {
+    input
+        .lines()
+        .map(|line| line.split_at(line.len() / 2))
+        .collect::<Vec<(&str, &str)>>()
+}
+
+fn get_shared_char(pair: (&str, &str)) -> char {
+    for c in pair.0.chars() {
+        if pair.1.contains(c) {
+            return c;
+        }
+    }
+
+    '_'
+}
+
+fn priority_of(c: &char) -> u32 {
+    match c {
+        'a'..='z' => (*c) as u32 - 96,
+        'A'..='Z' => (*c) as u32 - 38,
+        _ => 0,
+    }
 }
 
 #[cfg(test)]
@@ -26,8 +54,16 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw";
 
     #[test]
+    fn priority_is_calculated_correctly() {
+        assert_eq!(priority_of(&'a'), 1);
+        assert_eq!(priority_of(&'z'), 26);
+        assert_eq!(priority_of(&'A'), 27);
+        assert_eq!(priority_of(&'Z'), 52);
+    }
+
+    #[test]
     fn example_1() {
-        assert_eq!(part_1(INPUT), 0);
+        assert_eq!(part_1(INPUT), 157);
     }
 
     #[test]
